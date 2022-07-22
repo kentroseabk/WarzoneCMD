d5544801f9e650885901f5298785f05d9cba9b8d
kentroseabk <109536305+kentroseabk@users.noreply.github.com>
2022-07-22 09:47:16 -0500

    Add looting, fighting, moving

--- WarzoneCMD.cpp
+++ WarzoneCMD.cpp
@@ -8,29 +8,90 @@
 
 using namespace std;
 
-unsigned short int msTimeToSleep = 1;
-unsigned short int msToSleepFactor = 1;
-unsigned short int countdownTimer = 5;
+constexpr unsigned short int msTimeToSleep = 1;
+constexpr unsigned short int msToSleepFactor = 1;
+constexpr unsigned short int countdownTimerStart = 5;
+
+constexpr unsigned short int gasTimerStart = 3;
+unsigned short int gasTimerCurrent = gasTimerStart;
+
+unsigned short int playerMoves = 0;
+constexpr unsigned short int playerMovesLootFactor = 2;
+
+bool playerDead;
 
 unsigned short int enemiesKilled;
-constexpr unsigned short int enemyKillsToWin = 5;
+constexpr unsigned short int enemyKillsToWin = 3;
+
+float enemyHitChance = 0.25f;
+
+struct Location
+{
+    string name;
+    bool gassed;
+};
+
+Location airfield{ "Airfield", false };
+Location resort{ "Resort", false };
+Location capitol{ "Capitol", false };
+Location powerPlant{ "Power Plant", false };
+Location beachhead{ "Beachhead", false };
+
+Location* currentLocation;
 
 constexpr unsigned short int locationsSize = 5;
-string locations[locationsSize]{ "Airfield", "Resort", "Capital", "Power Plant", "Beachhead" };
+Location locations[locationsSize]{ airfield, resort, capitol, powerPlant, beachhead };
 
-constexpr unsigned short int locationActionSize = 2;
-string locationActions[locationActionSize]{ "Loot", "Move" };
+constexpr unsigned short int locationActionSize = 3;
+string locationActions[locationActionSize]{ "Fight", "Loot", "Move" };
 
 // location actions: loot, move, fight (if there's an enemy)
 // weapons: pistol (default), smg, assualt rifle, each has higher chance of downing enemy
+// chance for better loot goes up as player moves
 // win cond? kill 5 players
 // combat? random, but enemies in late game have higher chance of killing you
+// gas moves into random location every x turns
+
+struct Weapon
+{
+    string name;
+    float hitChance;
+
+    Weapon(string name, float hitChance) : name(name), hitChance(hitChance) {}
+};
 
 void DisplayIntro();
 unsigned short int GetValidInput(unsigned short int max);
 unsigned short int GetLocationSelection();
 unsigned short int GetLocationAction();
+Weapon SearchForLoot();
 void SleepForSeconds(unsigned short int seconds);
+void MainGameLoop();
+unsigned short int GetIndexOfWeapon(string weaponName);
+Location* FillRandomLocationWithGas();
+void HandleInvalidInput();
+void FillGas();
+
+// starting weapon
+Weapon pistol{ "Pistol", 0.1f };
+
+// lootable
+Weapon smg{ "SMG", 0.4f };
+Weapon ar{ "AR", 0.7f };
+Weapon sniper{ "Sniper Rifle", 0.9f };
+
+constexpr unsigned short int weaponsSize = 3;
+
+Weapon weapons[weaponsSize]{ smg, ar, sniper };
+
+Weapon currentWeapon = pistol;
+
+void HandleInvalidInput()
+{
+    cout << "Invalid input" << endl;
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
 
 unsigned short int GetValidInput(unsigned short int max)
 {
@@ -46,9 +107,7 @@ unsigned short int GetValidInput(unsigned short int max)
 
         if (cin.fail() || selection < 1 || selection > max)
         {
-            cout << "Invalid input" << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            HandleInvalidInput();
 
             retry = true;
         }
@@ -65,12 +124,34 @@ unsigned short int GetLocationSelection()
 
     for (int i = 0; i < locationsSize; i++)
     {
-        cout << (i + 1) << ". " << locations[i] << endl;
+        cout << (i + 1) << ". " << locations[i].name;
+        
+        if (locations[i].gassed)
+        {
+            cout << " (gassed)";
+        }
+        
+        cout << endl;
     }
 
     cout << endl;
 
-    unsigned short int selection = GetValidInput(locationsSize);
+    bool retry;
+
+    unsigned short int selection;
+
+    do
+    {
+        retry = false;
+
+        selection = GetValidInput(locationsSize);
+
+        if (locations[selection - 1].gassed)
+        {
+            HandleInvalidInput();
+            retry = true;
+        }
+    } while (retry);
 
     return selection;
 }
@@ -90,6 +171,49 @@ unsigned short int GetLocationAction()
 
     return selection;
 }
+
+unsigned short int GetUngassedLocationsCount()
+{
+    unsigned short int ungassedLocationsCount = 0;
+
+    for (int i = 0; i < locationsSize; i++)
+    {
+        if (!locations[i].gassed)
+        {
+            ungassedLocationsCount++;
+        }
+    }
+
+    return ungassedLocationsCount;
+}
+
+Location* FillRandomLocationWithGas()
+{
+    unsigned short int ungassedLocationsCount = GetUngassedLocationsCount();
+
+    int randomNumber = (rand() % ungassedLocationsCount);
+
+    unsigned short int ungassedLocationsCounter = 0;
+
+    Location* locationToGas = nullptr;
+
+    for (int i = 0; i < locationsSize; i++)
+    {
+        if (!locations[i].gassed)
+        {
+            if (randomNumber == ungassedLocationsCounter)
+            {
+                locationToGas = &locations[i];
+                break;
+            }
+
+            ungassedLocationsCounter++;
+        }
+    }
+
+    return locationToGas;
+}
+
 
 void DisplayIntro()
 {
@@ -111,7 +235,15 @@ void DisplayIntro()
 
     SleepForSeconds(msTimeToSleep);
 
-    cout << endl << "                       (command line edition)" << endl << endl;
+    cout << endl << "                           (command line edition)" << endl << endl;
+
+    SleepForSeconds(msTimeToSleep);
+
+    cout << endl << "Kill " << enemyKillsToWin << " enemies before the gas gets you." << endl;
+
+    SleepForSeconds(msTimeToSleep);
+
+    cout << endl << "Move to find better loot." << endl;
 
     SleepForSeconds(msTimeToSleep);
 
@@ -121,7 +253,7 @@ void DisplayIntro()
 
     SleepForSeconds(msTimeToSleep);
 
-    for (int i = countdownTimer; i > 0; i--)
+    for (int i = countdownTimerStart; i > 0; i--)
     {
         cout << i << endl;
         SleepForSeconds(msTimeToSleep);
@@ -130,10 +262,178 @@ void DisplayIntro()
     cout << endl;
 }
 
+// based on how much the player has moved
+Weapon SearchForLoot()
+{
+    // player moves, 0 to inf
+    int randomNumber = (rand() % 101) + (playerMoves * playerMovesLootFactor); // randomNumber in the range 0 to 100, with bonus for moving
+
+    // 0 - 30: nothing
+    // 30 - 60: smg
+    // 70 - 90: ar
+    // 90 - 100: sniper
+
+    if (randomNumber <= 30)
+    {
+        return currentWeapon;
+    }
+    else if (randomNumber <= 60)
+    {
+        return smg;
+    }
+    else if (randomNumber <= 90)
+    {
+        return ar;
+    }
+    else
+    {
+        return sniper;
+    }
+
+    return currentWeapon;
+}
+
+unsigned short int GetIndexOfWeapon(string weaponName)
+{
+    for (int i = 0; i < weaponsSize; i++)
+    {
+        if (weapons[i].name == weaponName)
+        {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
 void SleepForSeconds(unsigned short int seconds)
 {
     int timeToSleepMS = seconds * 1000 * msToSleepFactor;
     this_thread::sleep_for(std::chrono::milliseconds(timeToSleepMS));
+}
+
+void MainGameLoop()
+{
+    bool gameOver = false;
+
+    do 
+    {
+        cout << "Gas closing in... " << gasTimerCurrent << endl << endl;
+
+        SleepForSeconds(msTimeToSleep);
+
+        cout << "Enemies killed: " << enemiesKilled << "/" << enemyKillsToWin << endl << endl;
+
+        SleepForSeconds(msTimeToSleep);
+
+        unsigned short int locationActionSelection = GetLocationAction();
+
+        if (locationActionSelection == 1)
+        {
+            // fight
+            int randomNumber = (rand() % 101);
+
+            if (randomNumber < (int)(currentWeapon.hitChance * 100))
+            {
+                cout << "You killed an enemy." << endl << endl;
+                enemiesKilled++;
+
+                if (enemiesKilled >= enemyKillsToWin)
+                {
+                    cout << "You won!" << endl << endl;
+                    gameOver = true;
+                }
+            }
+            else
+            {
+                randomNumber = (rand() % 101);
+
+                if (randomNumber < (int)(enemyHitChance * 100))
+                {
+                    cout << "You were killed by an enemy." << endl << endl;
+                    gameOver = true;
+                }
+                else
+                {
+                    cout << "You both missed." << endl << endl;
+                }
+            }
+
+        }
+        else if (locationActionSelection == 2)
+        {
+            // loot
+            Weapon newWeapon = SearchForLoot();
+
+            if (newWeapon.name != currentWeapon.name && GetIndexOfWeapon(newWeapon.name) > GetIndexOfWeapon(currentWeapon.name))
+            {
+                cout << "Found " << newWeapon.name << ". Dropping " << currentWeapon.name << "." << endl << endl;
+                currentWeapon = newWeapon;
+            }
+            else
+            {
+                cout << "Nothing" << endl << endl;
+            }
+        }
+        else if (locationActionSelection == 3)
+        {
+            // move
+            unsigned short int locationSelection = GetLocationSelection();
+
+            cout << "Moving to " << locations[locationSelection - 1].name << endl << endl;
+
+            currentLocation = &locations[locationSelection - 1];
+
+            playerMoves++;
+        }
+
+        if (enemiesKilled >= enemyKillsToWin || playerDead)
+        {
+            gameOver = true;
+        }
+
+        gasTimerCurrent--;
+
+        if (gasTimerCurrent <= 0)
+        {
+            FillGas();
+
+            if (GetUngassedLocationsCount() == 0)
+            {
+                cout << "You died to the gas." << endl << endl;
+                gameOver = true;
+            }
+        }
+
+        SleepForSeconds(msTimeToSleep);
+
+    } while (!gameOver);
+}
+
+void FillGas()
+{
+    // fill random location w/ gas, if in that location, give selection to move
+    Location* locationToGas = FillRandomLocationWithGas();
+
+    locationToGas->gassed = true;
+
+    if (GetUngassedLocationsCount() == 0)
+    {
+        return;
+    }
+
+    if (locationToGas->name == currentLocation->name)
+    {
+        cout << "Location has been gassed. Please move." << endl << endl;
+
+        unsigned short int locationSelection = GetLocationSelection();
+
+        cout << "Moving to " << locations[locationSelection - 1].name << endl << endl;
+
+        currentLocation = &locations[locationSelection - 1];
+    }
+
+    gasTimerCurrent = gasTimerStart;
 }
 
 int main()
@@ -142,24 +442,13 @@ int main()
 
     unsigned short int locationSelection = GetLocationSelection();
 
-    cout << "Moving to " << locations[locationSelection - 1] << endl << endl;
+    cout << "Moving to " << locations[locationSelection - 1].name << endl << endl;
+
+    currentLocation = &locations[locationSelection - 1];
 
     SleepForSeconds(msTimeToSleep);
 
-    unsigned short int locationActionSelection = GetLocationAction();
-
-    if (locationActionSelection == 1)
-    {
-        // loot
-    }
-    else if (locationActionSelection == 2)
-    {
-        // move
-    }
-    else if (locationActionSelection == 3)
-    {
-        // fight
-    }
+    MainGameLoop();
 
     return 0;
 }
